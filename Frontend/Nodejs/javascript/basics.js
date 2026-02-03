a8b08b43f9fdd19c98ae9e2b38ded46ee3dac97b
psychotixk <itismesenpai@gmail.com>
2026-02-04 00:39:58 +0545

    javascrips basics

---
+++ Frontend/Nodejs/javascript/basics.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+//  *! Data Types
+//  *? string
+let name = "suman"
+
+//  *? number
+let num = 23;
+
+//  *? boolean
+let isTrue = false
+
+//  *? bigint
+
+//  *? undefined
+//  *? null
+//  *? symbol
+
+//  *? Objects
+let users = ["suman", "bibek", "aayush", "manish"]
+let user = {fname: "Suman", lname: "Kathayat"}
+
